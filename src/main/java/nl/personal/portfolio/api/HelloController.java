7fc67a5d726f6e7ff70e4cb1af74d64804fd70bb
Phi <anhphi099@gmail.com>
2023-12-04 15:07:29 +0100

    Fixes 404 issue

--- src/main/java/nl/personal/portfolio/api/HelloController.java
+++ src/main/java/nl/personal/portfolio/api/HelloController.java
@@ -1,16 +1,15 @@
 package nl.personal.portfolio.api;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
-    public String hello(){
-        return "hello world";
+    String hello(){
+        return "hello-page";
     }
-
 }
