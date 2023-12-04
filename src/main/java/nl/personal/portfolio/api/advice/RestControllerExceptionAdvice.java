178ab717a472f030da2977ed61345b325044840e
Phi <anhphi099@gmail.com>
2023-12-04 14:48:39 +0100

    Fixes not found when no static resource for path.

--- src/main/java/nl/personal/portfolio/api/advice/RestControllerExceptionAdvice.java
+++ src/main/java/nl/personal/portfolio/api/advice/RestControllerExceptionAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -14,19 +15,19 @@ public class RestControllerExceptionAdvice {
 
     @ExceptionHandler({ConstraintViolationException.class, HttpMessageNotReadableException.class})
     @ResponseStatus(BAD_REQUEST)
-    private void badRequest(RuntimeException e){
+    private void badRequest(){
 
     }
 
-    @ExceptionHandler(NoHandlerFoundException.class)
+    @ExceptionHandler({NoHandlerFoundException.class, NoResourceFoundException.class})
     @ResponseStatus(NOT_FOUND)
-    private void notFound(NoHandlerFoundException e){
+    private void notFound(){
 
     }
 
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(INTERNAL_SERVER_ERROR)
-    private void internalServerError(Exception e){
+    private void internalServerError(){
 
     }
 
