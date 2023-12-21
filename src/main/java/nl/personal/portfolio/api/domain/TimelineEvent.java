45a8ad3c86902a24fad1d2bde8758cbf3542a351
Phi Tiet <phi.tiet@rabobank.nl>
2023-12-21 16:44:20 +0100

    More work on timeline

--- src/main/java/nl/personal/portfolio/api/domain/TimelineEvent.java
+++ src/main/java/nl/personal/portfolio/api/domain/TimelineEvent.java
@@ -1,11 +1,30 @@
 package nl.personal.portfolio.api.domain;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
-public record TimelineEvent(String title, String description, String iconHtmlClass, LocalDate start, LocalDate end)
-        implements Comparable<TimelineEvent>  {
+import org.springframework.format.annotation.DateTimeFormat;
+
+public record TimelineEvent(
+        String employer,
+        String jobTitle,
+        String description,
+        String iconHtmlClass,
+        @DateTimeFormat(pattern = "dd-MM-yyyy")
+        LocalDate start,
+        @DateTimeFormat(pattern = "dd-MM-yyyy")
+        Optional<LocalDate> end)
+        implements Comparable<TimelineEvent> {
     @Override
     public int compareTo(TimelineEvent other) {
         return start.compareTo(other.start);
+    }
+
+    public String getWorkPeriod() {
+        var formatter = DateTimeFormatter.ofPattern("MM-yyyy");
+        var end = end().isPresent() ? end().get().format(formatter) : "current";
+        var start = start().format(formatter);
+        return end + " - " + start;
     }
 }
