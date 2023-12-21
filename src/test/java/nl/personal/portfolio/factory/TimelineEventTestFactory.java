0b07e07e2dd5c247155dcb6a162ecef11861baae
Phi Tiet <phi.tiet@rabobank.nl>
2023-12-21 16:44:20 +0100

    More work on timeline

--- src/test/java/nl/personal/portfolio/factory/TimelineEventTestFactory.java
+++ src/test/java/nl/personal/portfolio/factory/TimelineEventTestFactory.java
@@ -1,11 +1,12 @@
 package nl.personal.portfolio.factory;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import nl.personal.portfolio.api.domain.TimelineEvent;
 
 public class TimelineEventTestFactory {
     public static TimelineEvent defaultTimelineEvent(){
-        return new TimelineEvent("title", "description", "icon", LocalDate.now().minusDays(1), LocalDate.now());
+        return new TimelineEvent("employer", "jobTitle", "description", "icon", LocalDate.now().minusDays(1), Optional.of(LocalDate.now()));
     }
 }
