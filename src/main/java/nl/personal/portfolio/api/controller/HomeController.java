1c491c263c84d8331e2913716f7197c6cdb5d6f6
Phi Tiet <anhphi099@gmail.com>
2024-05-22 21:54:51 +0200

    Adds post redirect -> get to handle random cloudflare redirect errors

--- src/main/java/nl/personal/portfolio/api/controller/HomeController.java
+++ src/main/java/nl/personal/portfolio/api/controller/HomeController.java
@@ -5,7 +5,11 @@ import nl.personal.portfolio.core.HomePageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,7 +18,7 @@ public class HomeController {
 
     private final HomePageService homePageService;
 
-    @GetMapping()
+    @RequestMapping(method = { POST,  GET })
     public String home(Model model) {
         final var details = homePageService.getDetails();
         model.addAttribute("details", details);
